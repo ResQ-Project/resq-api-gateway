2bb89cbbae5804599210df331827e1626c73f719
Ravindu007 <ravindu0504@gmail.com>
2025-05-13 13:05:33 +0530

    initial commit of the API-Gateway

---
+++ src/main/java/com/resq/resq_api_gateway/ResqApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.resq.resq_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ResqApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resq/resq_api_gateway/ResqApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resq.resq_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
